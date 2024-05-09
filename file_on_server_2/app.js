d31fd48e973a70213298b70d5d84fe02e59d0dca
Nguyễn Đức Quân <87223367+335forever@users.noreply.github.com>
2024-05-09 14:32:26 +0700

    feature: API + database

--- file_on_server_2/app.js
+++ file_on_server_2/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const app = express();
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+require('dotenv').config();
 
 
 // Middleware để phân tích các request body dưới dạng JSON
@@ -17,76 +19,64 @@ app.use((req, res, next) => {
     next();
 });
 
-const dbConfig = {
-    host: 'localhost',
-    user: 'quan',
-    password: '335forever',
-    database: 'database_for_server2'
-};
+const dbConfig = JSON.parse(process.env.dbConfig);
 
-// Kiểm tra kết nối cơ sở dữ liệu
-function connectToDatabase() {
-    const conn = mysql.createConnection(dbConfig);
-
-    conn.connect((err) => {
-        if (err) {
-            console.error('Lỗi kết nối cơ sở dữ liệu:', err);
-            return;
-        }
-        console.log('Kết nối thành công đến cơ sở dữ liệu');
-    });
-
-    return conn;
-}
+// Tạo một pool kết nối
+const pool = mysql.createPool(dbConfig);
 
 // Tạo token khi đăng nhập thành công và lưu vào cơ sở dữ liệu
-function generateToken(username) {
-    const token = require('crypto').randomBytes(32).toString('hex');
+const generateToken = async (username) => {
+    const token = crypto.randomBytes(32).toString('hex');
 
-    const conn = connectToDatabase();
+    try {
+        // Lấy một kết nối từ pool
+        const connection =  await pool.getConnection();
+        
+        const tokenCreatedAt = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        
+        const [rows, fields] = await connection.execute(
+            "UPDATE m_account SET token = ?, token_created_at = ? WHERE username = ?",
+            [token, tokenCreatedAt, username]
+        );
+        connection.release();
+    } catch (err) {
+        console.error('Lỗi khi cập nhật token vào cơ sở dữ liệu:', err);
+    }
 
-    const tokenCreatedAt = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    const sql = "UPDATE m_account SET token = ?, token_created_at = ? WHERE username = ?";
-    conn.query(sql, [token, tokenCreatedAt, username], (err, result) => {
-        if (err) {
-            console.error('Lỗi khi cập nhật token vào cơ sở dữ liệu:', err);
-            return;
-        }
-        console.log('Token được cập nhật thành công');
-    });
-
-    conn.end();
     return token;
 }
 
 // Kiểm tra và xác minh token
-function verifyToken(token) {
-    const conn = connectToDatabase();
-
-    const sql = "SELECT * FROM m_account WHERE token = ? AND token_created_at >= DATE_SUB(NOW(), INTERVAL 1 DAY)";
-    conn.query(sql, [token], (err, result) => {
-        if (err) {
-            console.error('Lỗi khi thực hiện truy vấn:', err);
-            return false;
-        }
-        if (result && result.length > 0) {
-            return result[0].username;
+const verifyToken = async (token) => {
+    try {
+        // Lấy một kết nối từ pool
+        const connection = await pool.getConnection();
+        
+        const [rows, fields] = await connection.execute(
+            "SELECT * FROM m_account WHERE token = ? AND token_created_at >= DATE_SUB(NOW(), INTERVAL 1 DAY)",
+            [token]
+        );
+        
+        connection.release();
+        
+        if (rows && rows.length > 0) {
+            return rows[0].username;
         } else {
             return false;
         }
-    });
-
-    conn.end();
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 // Xác thực token
-function authenticate(req, res, next) {
+const authenticate = async (req, res, next) => {
     const token = req.headers['authorization'];
     if (!token) {
         res.status(401).json({ status: 'error', message: 'Unauthorized' });
         return;
     }
-    const username = verifyToken(token.replace('Bearer ', ''));
+    const username = await verifyToken(token.replace('Bearer ', ''));
     if (!username) {
         res.status(401).json({ status: 'error', message: 'Unauthorized' });
         return;
@@ -95,36 +85,59 @@ function authenticate(req, res, next) {
     next();
 }
 
+const handleInsertResult = (result) => {
+    if (result && result.affectedRows === 1) {
+        return {
+            success: true,
+            message: "Insert successful",
+        };
+    } else {
+        return {
+            success: false,
+            message: "Insert failed"
+        };
+    }
+};
+
 // Đăng nhập
-app.post('/api/signin', (req, res) => {
-    const { username, password } = req.body;
+app.post('/api/signin', async (req, res) => {
+    const userData = req.body;
+    if (userData.username && userData.password) {
+        const username = userData.username;
+        const password = userData.password;
+    
+        try {
+            // Lấy một kết nối từ pool
+            const connection = await pool.getConnection();
+            
+            const [rows, fields] = await connection.execute(
+                "SELECT * FROM m_account WHERE username = ?",
+                [username]
+            );
 
-    const conn = connectToDatabase();
-
-    const sql = "SELECT * FROM m_account WHERE username = ?";
-    conn.query(sql, [username], (err, result) => {
-        if (err) {
-            res.status(500).json({ status: 'error', message: 'Database unreachable' });
-            return;
-        }
-        if (result && result.length > 0) {
-            const hashed_password = result[0].password;
-            if (password === hashed_password) {
-                const token = generateToken(username);
-                res.json({ token });
+            if (rows && rows.length > 0) {
+                const hashed_password_saved = rows[0].password;
+                if (bcrypt.compareSync(password, hashed_password_saved)) {
+                    const token = await generateToken(username);
+                    res.json({token});
+                } else {
+                    res.status(401).json({ status: 'error', message: 'Invalid password' });
+                }
             } else {
-                res.status(401).json({ status: 'error', message: 'Invalid password' });
+                res.status(404).json({ status: 'error', message: 'Invalid username' });
             }
-        } else {
-            res.status(404).json({ status: 'error', message: 'Invalid username' });
+            connection.release();
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ status: 'error', message: err });
         }
-    });
-
-    conn.end();
+    } else {
+        res.status(400).json({ status: 'error', message: 'Missing username or password' });
+    }
 });
 
 // Đăng ký
-app.post('/api/signup', (req, res) => {
+app.post('/api/signup', async (req, res) => {
     // Lấy dữ liệu từ yêu cầu
     const userData = req.body;
 
@@ -132,71 +145,123 @@ app.post('/api/signup', (req, res) => {
     if (userData.username && userData.password) {
         const username = userData.username;
         const password = userData.password;
-
-        // Kết nối đến cơ sở dữ liệu
-        const conn = connectToDatabase();
-        if (conn.connect_error) {
-            res.status(500).json({ status: 'error', message: 'Database unreachable' });
-            return;
-        }
-
-        // Kiểm tra xem người dùng đã tồn tại chưa
-        conn.query('SELECT * FROM m_account WHERE username = ?', [username], (error, results) => {
-            if (error) {
-                res.status(500).json({ status: 'error', message: 'Failed to create user' , error1 : "error"});
-                return;
-            }
-
-            if (results.length > 0) {
-                // Người dùng đã tồn tại
+        
+        try {
+            // Lấy một kết nối từ pool
+            const connection = await pool.getConnection();
+            
+            const [rows, fields] = await connection.execute(
+                'SELECT * FROM `m_account` WHERE username = ?',
+                [username]
+            );
+    
+            if (rows.length > 0) {
                 res.status(409).json({ status: 'error', message: 'Existed username' });
+                
             } else {
                 // Người dùng chưa tồn tại, thêm vào cơ sở dữ liệu
                 const name = userData.name || 'Your Name';
                 const email = userData.email || null;
                 const dob = userData.dob || null;
-                const isShoper = userData.isShoper === 'true';
                 const phone = userData.phone || null;
                 const address = userData.address || null;
 
-                // Tạo câu lệnh SQL và thực thi
-                const sql = `INSERT INTO m_account (username, password, name, email, phone, address, birthday, isShoper) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-                const hashedPassword = bcrypt.hashSync(password, 10); // Sử dụng bcrypt để băm mật khẩu
-                conn.query(sql, [username, hashedPassword, name, email, phone, address, dob, isShoper], (error, results) => {
-                    if (error) {
-                        res.status(500).json({ status: 'error', message: 'Failed to create user' });
-                        return;
-                    }
-
-                    if (isShoper) {
-                        // Thêm dữ liệu vào bảng M_shoper
-                        const accountId = results.insertId; // Lấy ID của tài khoản vừa được tạo
-                        const taxid = userData.taxid || null;
-                        conn.query('INSERT INTO m_shoper (taxid, accountID) VALUES (?, ?)', [taxid, accountId], (error, results) => {
-                            if (error) {
-                                res.status(500).json({ status: 'error', message: 'Failed to create user' });
-                                return;
-                            }
-                        });
-                    }
-
-                    // Trả về thông báo thành công
-                    res.status(201).json({ status: 'success', message: 'Sign up successfully' });
-                });
+                try {
+                    const hashedPassword = bcrypt.hashSync(password, 10); // Sử dụng bcrypt để băm mật khẩu
+                    const [result, fields] = await connection.execute(
+                        `INSERT INTO m_account (username, password, name, email, phone, address, birthday) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+                        [username, hashedPassword, name, email, phone, address, dob]
+                    );
+                    const response = handleInsertResult(result);
+                    
+                    if (response.success) res.status(200).json({ status: 'success', message: response });
+                    else res.status(500).json({ status: 'error', message: response });
+                } catch (err) {
+                    res.status(500).json({ status: 'error', message: err });
+                }
             }
-        });
-
-        // Đóng kết nối
-        conn.end();
+            
+            connection.release();
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ status: 'error', message: 'Failed to create user'});
+        }
     } else {
         res.status(400).json({ status: 'error', message: 'Missing username or password' });
     }
 });
 
-
 // Cập nhật thông tin người dùng
-app.put('/api/update', authenticate, (req, res) => {
-    // TODO: Thực hiện việc cập nhật thông tin người dùng
+app.put('/api/update', authenticate, async (req, res) => {
+    const newData = req.body;
+
+    if (Object.keys(newData).length > 0) {
+        try {
+            const username = req.username;
+
+            let updateQuery = 'UPDATE m_account SET';
+            const updateValues = [];
+
+            Object.keys(newData).forEach(key => {
+                if (key !== 'username') { 
+                    if (key === 'password') {
+                        updateQuery += ` ${key} = ?,`;
+                        updateValues.push(bcrypt.hashSync(newData[key], 10));
+                    } 
+                    else if (key === 'isShoper') {
+                        updateQuery += ` ${key} = ?,`;
+                        updateValues.push(newData[key] === 'true');
+                    } else {
+                        updateQuery += ` ${key} = ?,`;
+                        updateValues.push(newData[key]);
+                    }
+                }
+            });
+
+            updateQuery = updateQuery.slice(0, -1) + ' WHERE username = ?';
+            updateValues.push(username);
+
+            const connection = await pool.getConnection();
+        
+            const [result, fields] = await connection.execute(
+                updateQuery,
+                updateValues
+            );
+
+            if (result && result.affectedRows === 1)
+                res.status(200).json({ message: 'Updated successfully' });
+            else res.status(500).json({ error: 'Updated fail' });
+            
+            connection.release();
+        } catch (error) {
+            console.error('Updated fail:', error);
+            res.status(500).json({ error: 'Updated fail' });
+        }
+    } else {
+        res.status(400).json({ error: 'None infor' });
+    }
+});
+
+// Lấy thông tin người dùng
+app.get('/api/getuserinfo', authenticate, async (req, res) => {
+    const username = req.username;
+    try {
+        const connection = await pool.getConnection();
+
+        const [rows, fields] = await connection.execute(
+            "SELECT username, name, email, phone, address, birthday, isShoper FROM m_account WHERE username = ?",
+            [username]
+        );
+
+        if (rows.length === 1) 
+            res.status(200).json( rows[0] );
+        else res.status(500).json({ error: 'Get infor fail' });
+
+        connection.release();
+    } catch (error) {
+        console.error('Get infor fail:', error);
+        res.status(500).json({ error: 'Get infor fail' });
+    }
 });
 
 // Khởi động server
@@ -204,3 +269,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
