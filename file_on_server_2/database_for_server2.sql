c53d0935434e6f5727f9405966044a17d049a794
Nguyễn Đức Quân <87223367+335forever@users.noreply.github.com>
2024-06-06 03:33:27 +0700

    update mysql backup file

--- file_on_server_2/database_for_server2.sql
+++ file_on_server_2/database_for_server2.sql
@@ -35,7 +35,7 @@ CREATE TABLE `m_account` (
   `token` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`accountid`),
   UNIQUE KEY `username_uniq` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `m_account` (
 
 LOCK TABLES `m_account` WRITE;
 /*!40000 ALTER TABLE `m_account` DISABLE KEYS */;
-INSERT INTO `m_account` VALUES (34,'quan','$2b$10$2nQ7PaCyyQabWki1yJCbquwItheEyXj8Lvjmy8734DV3ub5thAHBG','quan','ducquank52t1@gmail.com','0941672663','Ha Noi','2002-06-22',_binary '','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1YW4iLCJpYXQiOjE3MTY0NDgyODcsImV4cCI6MTcxNjQ1OTA4N30.NQb7SFnpxM_6N2GOaZSZSLA1yp2fSy4zCwnCdsnFafA'),(39,'quan2205','$2b$10$ryIdarkmHXTfULXESx1lMeInes3HI2B565iKQ1ji0CHoNvN/e342G','NDQ',NULL,NULL,NULL,NULL,_binary '','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1YW4yMjA1IiwiaWF0IjoxNzE2MzkxMjI3LCJleHAiOjE3MTYzOTQ4Mjd9.LHIW1_Z_NvI52uYCt4Zf7hKUt5Unq7cT4zL-6lMRu9g'),(40,'quan2205-01','$2b$10$XBxXhg.XSRGuIocVLAQ8a.UmobSv53OmynzKz7Jbzlt6zQ47vzPdm','Your Name',NULL,'0941672663','Ha Noi',NULL,_binary '','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1YW4yMjA1LTAxIiwiaWF0IjoxNzE2Mzk1MzgwLCJleHAiOjE3MTY0MDYxODB9.RbMN8rSrefUU-WdNhXgbTguzoUAMen2S9O9uMQ6uJLQ'),(43,'quan2205-03','$2b$10$24NHznITG5aeS22.LL70s.YSOg3ASDhPbwUAuUz61IJ9Ox4Av6fgC','quan','ducquank52t1@gmail.com','0941672663','Ha Noi','2002-06-22',_binary '','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1YW4yMjA1LTAzIiwiaWF0IjoxNzE2NDA1NDQ5LCJleHAiOjE3MTY0MTYyNDl9._MwxFtG6FbvYZVWDtkBIMS-pGCvHlm1OblcfvktjRlw'),(44,'quan2205-04','$2b$10$GFgaeNXWeKl1aNIdeKqaC.Dnv74oO4UeQ0mvlMdB59qi6PusGF1bS','Your Name',NULL,NULL,NULL,NULL,_binary '\0',NULL),(45,'quan2205-05','$2b$10$WKZmhUYh8WOKXL/.XciATekYCHH8YM0bLblGpfQ.G1fn2mMwS9WZ2','NDQ',NULL,'0941672663',NULL,NULL,_binary '\0',NULL),(46,'quan2205-06','$2b$10$kaw6DuZOL34RcMMPePmkBeUKoKeonM7jD0ZvllphG0zNC51ECVOAG','NDQ',NULL,'0941672663',NULL,NULL,_binary '\0',NULL);
+INSERT INTO `m_account` VALUES (34,'quan','$2b$10$2nQ7PaCyyQabWki1yJCbquwItheEyXj8Lvjmy8734DV3ub5thAHBG','quan','ducquank52t1@gmail.com','0941672663','Ha Noi','2002-06-22',_binary '','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1YW4iLCJpYXQiOjE3MTY4MDMxMTUsImV4cCI6MTcxNjgxMzkxNX0.Ic5hvmxy1oV0y8LKaSv_YTgXtflElWU7dL18f2qsvcc'),(39,'quan2205','$2b$10$ryIdarkmHXTfULXESx1lMeInes3HI2B565iKQ1ji0CHoNvN/e342G','NDQ',NULL,NULL,NULL,NULL,_binary '','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1YW4yMjA1IiwiaWF0IjoxNzE2MzkxMjI3LCJleHAiOjE3MTYzOTQ4Mjd9.LHIW1_Z_NvI52uYCt4Zf7hKUt5Unq7cT4zL-6lMRu9g'),(40,'quan2205-01','$2b$10$XBxXhg.XSRGuIocVLAQ8a.UmobSv53OmynzKz7Jbzlt6zQ47vzPdm','Your Name',NULL,'0941672663','Ha Noi',NULL,_binary '','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1YW4yMjA1LTAxIiwiaWF0IjoxNzE2Mzk1MzgwLCJleHAiOjE3MTY0MDYxODB9.RbMN8rSrefUU-WdNhXgbTguzoUAMen2S9O9uMQ6uJLQ'),(43,'quan2205-03','$2b$10$24NHznITG5aeS22.LL70s.YSOg3ASDhPbwUAuUz61IJ9Ox4Av6fgC','quan','ducquank52t1@gmail.com','0941672663','Ha Noi','2002-06-22',_binary '','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1YW4yMjA1LTAzIiwiaWF0IjoxNzE2NDA1NDQ5LCJleHAiOjE3MTY0MTYyNDl9._MwxFtG6FbvYZVWDtkBIMS-pGCvHlm1OblcfvktjRlw'),(44,'quan2205-04','$2b$10$GFgaeNXWeKl1aNIdeKqaC.Dnv74oO4UeQ0mvlMdB59qi6PusGF1bS','Your Name',NULL,NULL,NULL,NULL,_binary '\0',NULL),(45,'quan2205-05','$2b$10$WKZmhUYh8WOKXL/.XciATekYCHH8YM0bLblGpfQ.G1fn2mMwS9WZ2','NDQ',NULL,'0941672663',NULL,NULL,_binary '\0',NULL),(46,'quan2205-06','$2b$10$kaw6DuZOL34RcMMPePmkBeUKoKeonM7jD0ZvllphG0zNC51ECVOAG','NDQ',NULL,'0941672663',NULL,NULL,_binary '\0',NULL),(47,'quan0306-01','$2b$10$sK15a7.5qKEttrI3xRVAG.o/dJVcatFlhOyKnGU5AzrRRvLqiprVq','NDQ',NULL,'0941672663',NULL,NULL,_binary '\0','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1YW4wMzA2LTAxIiwiaWF0IjoxNzE3NDAwMDQ0LCJleHAiOjE3MTc0MTA4NDR9.isw9fRvrWpzzYSppAof48Taeu3GlIzvjmRE2JsAEjn8');
 /*!40000 ALTER TABLE `m_account` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -112,10 +112,11 @@ CREATE TABLE `m_order` (
   `productID` int DEFAULT NULL,
   `quantity` int DEFAULT NULL,
   `voucherID` int DEFAULT NULL,
-  `isPAID` bit(1) DEFAULT NULL,
-  `TOTAL` int DEFAULT NULL,
-  `order_date` timestamp NULL DEFAULT NULL,
+  `isPaid` bit(1) DEFAULT NULL,
+  `total` bigint DEFAULT NULL,
+  `orderDate` timestamp NULL DEFAULT NULL,
   `status` int DEFAULT NULL,
+  `paymentMethod` int DEFAULT NULL,
   PRIMARY KEY (`orderID`),
   KEY `accountID` (`accountID`),
   KEY `productID` (`productID`),
@@ -123,7 +124,7 @@ CREATE TABLE `m_order` (
   CONSTRAINT `M_order_ibfk_1` FOREIGN KEY (`accountID`) REFERENCES `m_account` (`accountid`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `M_order_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `m_product` (`productID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `M_order_ibfk_3` FOREIGN KEY (`voucherID`) REFERENCES `m_voucher` (`voucherID`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,8 +133,45 @@ CREATE TABLE `m_order` (
 
 LOCK TABLES `m_order` WRITE;
 /*!40000 ALTER TABLE `m_order` DISABLE KEYS */;
+INSERT INTO `m_order` VALUES (7,34,34,2,7,_binary '\0',236000,'2024-05-27 09:48:56',0,0),(8,34,34,2,7,_binary '\0',236000,'2024-05-27 09:56:03',0,0),(9,34,34,2,7,_binary '\0',236000,'2024-05-27 09:59:11',0,0);
 /*!40000 ALTER TABLE `m_order` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`quan`@`localhost`*/ /*!50003 TRIGGER `update_voucher_quantity` AFTER INSERT ON `m_order` FOR EACH ROW BEGIN
+    IF NEW.voucherID != -1 THEN
+        UPDATE m_voucher SET quantity = quantity - 1 WHERE voucherID = NEW.voucherID;
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`quan`@`localhost`*/ /*!50003 TRIGGER `update_product_quantity` AFTER INSERT ON `m_order` FOR EACH ROW BEGIN
+    UPDATE m_product SET quantity = quantity - NEW.quantity WHERE productID = NEW.productID;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `m_product`
@@ -163,7 +201,7 @@ CREATE TABLE `m_product` (
 
 LOCK TABLES `m_product` WRITE;
 /*!40000 ALTER TABLE `m_product` DISABLE KEYS */;
-INSERT INTO `m_product` VALUES (34,'Set bộ nữ Banamo Fashion set áo tơ đũi hai lớp dây áo bản chun to 128',301,128000,9,6),(40,'Set bộ nữ Banamo Fashion set áo tơ đũi hai lớp dây áo bản chun to 129',253,129000,9,12),(41,'Set bộ nữ Banamo Fashion set áo tơ đũi hai lớp dây áo bản chun to 129',0,129000,9,13),(42,'Bút Màu 12/24/36/48/60 Acrylic Marker Vẽ Trên Mọi Chất Liệu Có Thể Vẽ Chồng Màu, Màu Sắc Tươi Sáng Dễ Sử Dụng',2548,25000,14,13),(43,'Bút Màu 12/24/36/48/60 Acrylic Marker Vẽ Trên Mọi Chất Liệu Có Thể Vẽ Chồng Màu, Màu Sắc Tươi Sáng Dễ Sử Dụng',2548,25000,14,14),(46,'Bút Màu 12/24/36/48/60 Acrylic Marker Vẽ Trên Mọi Chất Liệu Có Thể Vẽ Chồng Màu, Màu Sắc Tươi Sáng Dễ Sử Dụng',2548,25000,14,6);
+INSERT INTO `m_product` VALUES (34,'Set bộ nữ Banamo Fashion set áo tơ đũi hai lớp dây áo bản chun to 128',299,128000,9,6),(40,'Set bộ nữ Banamo Fashion set áo tơ đũi hai lớp dây áo bản chun to 129',253,129000,9,12),(41,'Set bộ nữ Banamo Fashion set áo tơ đũi hai lớp dây áo bản chun to 129',0,129000,9,13),(42,'Bút Màu 12/24/36/48/60 Acrylic Marker Vẽ Trên Mọi Chất Liệu Có Thể Vẽ Chồng Màu, Màu Sắc Tươi Sáng Dễ Sử Dụng',2548,25000,14,13),(43,'Bút Màu 12/24/36/48/60 Acrylic Marker Vẽ Trên Mọi Chất Liệu Có Thể Vẽ Chồng Màu, Màu Sắc Tươi Sáng Dễ Sử Dụng',2548,25000,14,14),(46,'Bút Màu 12/24/36/48/60 Acrylic Marker Vẽ Trên Mọi Chất Liệu Có Thể Vẽ Chồng Màu, Màu Sắc Tươi Sáng Dễ Sử Dụng',2548,25000,14,6);
 /*!40000 ALTER TABLE `m_product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -260,13 +298,14 @@ CREATE TABLE `m_voucher` (
   `voucherID` int NOT NULL AUTO_INCREMENT,
   `shoperID` int DEFAULT NULL,
   `discountPercent` int DEFAULT NULL,
-  `expried` date DEFAULT NULL,
+  `expired` date DEFAULT NULL,
   `minprice` int DEFAULT NULL,
   `maxdiscount` int DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
   PRIMARY KEY (`voucherID`),
   KEY `shoperID` (`shoperID`),
   CONSTRAINT `M_voucher_ibfk_1` FOREIGN KEY (`shoperID`) REFERENCES `m_shoper` (`shoperID`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -275,6 +314,7 @@ CREATE TABLE `m_voucher` (
 
 LOCK TABLES `m_voucher` WRITE;
 /*!40000 ALTER TABLE `m_voucher` DISABLE KEYS */;
+INSERT INTO `m_voucher` VALUES (4,6,10,'2024-05-29',-1,-1,0),(5,12,5,'2024-05-30',-1,-1,200),(6,6,15,'2024-05-31',-1,-1,1000),(7,6,10,'2024-05-31',-1,20000,998),(8,6,10,'2024-05-31',300000,20000,1000);
 /*!40000 ALTER TABLE `m_voucher` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -287,4 +327,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-23 15:24:40
+-- Dump completed on 2024-06-06  3:32:42
