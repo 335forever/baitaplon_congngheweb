bc0e1f225a08b247fc9060c0d647c45000495fb5
Bui Anh Quoc <p.lyquangk.lythedan@gmail.com>
2024-06-09 07:24:51 +0700

    Edit UI manage-products

--- client/apps/manage-products/src/components/Product.js
+++ client/apps/manage-products/src/components/Product.js
@@ -3,20 +3,18 @@ import icTrash from "../../assets/images/ic_trash.svg";
 
 export default function Product({ id, image, name, price }) {
   return (
-    <button>
-      <div className="product">
-        <div className="info">
-          <img src={image} alt=""></img>
-        </div>
-        <div className="function">
-          <button><img src={icEdit} alt="" /></button>
-          <button><img src={icTrash} alt="" /></button>
-        </div>
-        <div className="name">
-          <div>{name}</div>
-          <div style={{ "fontSize": "14px", "fontWeight": "700" }}>{price.toLocaleString('vi-VN')} đ</div>
-        </div>
+    <div className="product">
+      <div className="info">
+        <img src={image} alt=""></img>
       </div>
-    </button>
+      <div className="function">
+        <button><img src={icEdit} alt="" /></button>
+        <button><img src={icTrash} alt="" /></button>
+      </div>
+      <div className="name">
+        <div>{name}</div>
+        <div style={{ "fontSize": "14px", "fontWeight": "700" }}>{price.toLocaleString('vi-VN')} đ</div>
+      </div>
+    </div>
   )
 }
