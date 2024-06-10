89440a9e3b7ee65e1f087d52790494d6c9100e01
sclearner <71807587+sclearner@users.noreply.github.com>
2024-06-10 21:49:16 +0700

    feat: Link Quốc's UI to root-config

--- client/apps/account/src/account.component.js
+++ client/apps/account/src/account.component.js
@@ -1,16 +1,23 @@
-import './index.css';
+import "./index.css";
 
 import React, { useState, useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-// import { toast } from '@TachMonShop/notification';
+import { toast } from "@TachMonShop/notification";
 
-import MyProfile from './components/MyProfile';
-import ChangePassword from './components/ChangePassword';
-import ShopProfile from './components/ShopProfile';
-import { changePassword, getShopInfo, getUserInfo, upToShop, update, updateShop } from '../../../api/src/controllers/account.controller';
+import MyProfile from "./components/MyProfile";
+import ChangePassword from "./components/ChangePassword";
+import ShopProfile from "./components/ShopProfile";
+import {
+  changePassword,
+  getShopInfo,
+  getUserInfo,
+  upToShop,
+  updateUser,
+  updateShop,
+} from "@TachMonShop/api";
 
 export default function Account(props) {
-  document.title = "TachMonShop | Tài khoản"
+  document.title = "TachMonShop | Tài khoản";
 
   const [menu, setMenu] = React.useState(0);
   const [profile, setProfile] = React.useState();
@@ -31,67 +38,173 @@ export default function Account(props) {
   }
 
   useEffect(() => {
-    getProfile()
+    getProfile();
   }, []);
 
-
   const updateProfile = (_profile) => {
-    update(_profile, () => { /* toast*/ }, () => {/* toast*/ });
-  }
+    updateUser(
+      _profile,
+      () => {
+        toast({
+          title: "Cập nhật thành công!",
+          duration: 1000,
+          isClosable: false,
+          status: "success",
+        });
+      },
+      () => {
+        toast({
+          title: "Lỗi",
+          description: "Đã có lỗi xảy ra",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    );
+  };
 
   const chgPassword = (passwordSet) => {
-    changePassword(passwordSet,
-      () => {/*toast success*/ },
+    changePassword(
+      passwordSet,
+      () => {
+        toast({
+          title: "Cập nhật thành công!",
+          duration: 1000,
+          isClosable: false,
+          status: "success",
+        });
+      },
       (err) => {
-        if (err.statuscode == 401) {
+        if (err.response.status == 401) {
           // Toast warnign wrong old password
+          toast({
+            title: "Lỗi",
+            description: "Mật khẩu hiện tại không đúng",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
         }
-      })
-  }
+        else {
+          toast({
+            title: "Lỗi",
+            description: "Đã có lỗi xảy ra",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
+        }
+      }
+    );
+  };
 
   const updShop = (_profile) => {
-    if (profile.isShoper) updateShop(_profile, () => {/* toast success*/ }, () => {/*toast error*/ });
+    if (profile.isShoper)
+      updateShop(
+        _profile,
+        () => {
+          toast({
+            title: "Cập nhật thành công!",
+            duration: 1000,
+            isClosable: false,
+            status: "success",
+          });
+        },
+        () => {
+          toast({
+            title: "Lỗi",
+            description: "Đã có lỗi xảy ra",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
+        }
+      );
     else
-      upToShop(_profile, () => {/* toast success*/ }, () => {/*toast error*/ });
-  }
-
+      upToShop(
+        _profile,
+        () => {
+          toast({
+            title: "Chúc mừng bạn đã trở thành đối tác của chúng tôi",
+            duration: 1000,
+            isClosable: false,
+            status: "success",
+          });
+        },
+        () => {
+          toast({
+            title: "Lỗi",
+            description: "Đã có lỗi xảy ra",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
+        }
+      );
+  };
 
   return (
     <ChakraProvider>
       <div id="wrapper">
         <div id="route">
-          <div style={{ "flexGrow": "1" }}>
-            <a style={{ color: 'rgba(0, 0, 0, 0.5)' }}>Tài khoản </a>
-            /
+          <div style={{ flexGrow: "1" }}>
+            <a style={{ color: "rgba(0, 0, 0, 0.5)" }}>Tài khoản </a>/
             <a> My Account</a>
           </div>
           <div>Welcome!</div>
-          <div style={{ "color": "#DB4444" }}>{profile ? profile.name : ''}</div>
+          <div style={{ color: "#DB4444" }}>{profile ? profile.name : ""}</div>
         </div>
         <>
-          {profile && <div id="body">
-            <div className="menu">
-              <div className="menu-title">Manage My Account</div>
-              <div className="menu-option">
-                <a onClick={() => setMenu(0)} style={{ "color": menu == 0 ? "#db4444" : "rgba(0,0,0,0.5)" }}>My Profile</a>
-                <a onClick={() => setMenu(1)} style={{ "color": menu == 1 ? "#db4444" : "rgba(0,0,0,0.5)" }}>Change password</a>
+          {profile && (
+            <div id="body">
+              <div className="menu">
+                <div className="menu-title">Manage My Account</div>
+                <div className="menu-option">
+                  <a
+                    onClick={() => setMenu(0)}
+                    style={{ color: menu == 0 ? "#db4444" : "rgba(0,0,0,0.5)" }}
+                  >
+                    My Profile
+                  </a>
+                  <a
+                    onClick={() => setMenu(1)}
+                    style={{ color: menu == 1 ? "#db4444" : "rgba(0,0,0,0.5)" }}
+                  >
+                    Change password
+                  </a>
+                </div>
+                <div className="menu-title">My Shop</div>
+                <div className="menu-option">
+                  <a
+                    onClick={() => setMenu(2)}
+                    style={{ color: menu == 2 ? "#db4444" : "rgba(0,0,0,0.5)" }}
+                  >
+                    Shop profile
+                  </a>
+                  {/* {profile.isShoper ? <a onClick={() => setMenu(3)} style={{ "color": menu == 3 ? "#db4444" : "rgba(0,0,0,0.5)" }}>Unregister shop</a> : <></>} */}
+                </div>
               </div>
-              <div className="menu-title">My Shop</div>
-              <div className="menu-option">
-                <a onClick={() => setMenu(2)} style={{ "color": menu == 2 ? "#db4444" : "rgba(0,0,0,0.5)" }}>Shop profile</a>
-                {/* {profile.isShoper ? <a onClick={() => setMenu(3)} style={{ "color": menu == 3 ? "#db4444" : "rgba(0,0,0,0.5)" }}>Unregister shop</a> : <></>} */}
+              <div id="manage">
+                {menu == 0 ? (
+                  profile && (
+                    <MyProfile
+                      {...profile}
+                      onSubmit={(e) => updateProfile(e)}
+                    />
+                  )
+                ) : menu == 1 ? (
+                  <ChangePassword {...shop} onSubmit={(e) => chgPassword(e)} />
+                ) : menu == 2 ? (
+                  <ShopProfile {...shop} onSubmit={(e) => updShop(e)} />
+                ) : (
+                  <></>
+                )}
               </div>
             </div>
-            <div id="manage">
-              {menu == 0 ? profile && <MyProfile {...profile} onSubmit={(e) => updateProfile(e)} />
-                : menu == 1 ? < ChangePassword {...shop} onSubmit={(e) => chgPassword(e)} />
-                  : menu == 2 ? <ShopProfile {...shop} onSubmit={(e) => updShop(e)} />
-                    : <></>
-              }
-            </div>
-          </div>}
+          )}
         </>
       </div>
-    </ChakraProvider >
+    </ChakraProvider>
   );
 }
